02a5c2fbf1b7f16bae3c44fc51fb1ba5dc81f083
Zabee <qavi@e.org>
2017-12-10 19:26:32 +0530

    Made server to accept multiple connections and also added proper console comments. Log4j2 for logging is coming soon.

--- src/com/socket/ClientThread.java
+++ src/com/socket/ClientThread.java
@@ -3,6 +3,7 @@ package com.socket;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -25,7 +26,7 @@ class ProcessHandler extends Thread {
 		File file = null;
 		try {
 			Properties prop = new Properties();
-			file = new File(System.getProperty("user.dir") + "\\system.properties");
+			file = new File(System.getProperty("user.dir") + "\\System.properties");
 			prop.load(new FileInputStream(file));
 			serverIP = (String) prop.get("serverIP");
 			targetFile = prop.getProperty("targetBatFile");
@@ -50,13 +51,17 @@ class ProcessHandler extends Thread {
 					if ("run".equalsIgnoreCase(receiveMessage)
 							&& (process == null || (process != null && !process.isAlive()))) {
 						process = Runtime.getRuntime().exec("cmd /c " + targetFile);
-						if (process != null)
+						if (process != null) {
 							System.out.println("The windows batch file running!!");
+							System.out.println("Client terminated!!");
+							System.exit(0);			
+						}
 					}
 
 				}
-			} catch (Exception e) {
+			} catch (IOException e) {
 				System.out.println("Check the server connection and start client app again!!");
+				System.exit(0);
 			}
 		}
 	}
--- src/com/socket/RemoteCommandExecutor.java
+++ src/com/socket/RemoteCommandExecutor.java
@@ -7,11 +7,34 @@ public class RemoteCommandExecutor {
 
 	private static ServerSocket serverSocket;
 
-	public static void main(String[] args) {
-		try {
-			serverSocket = new ServerSocket(3000);
+	public static void main(String[] args) throws IOException {
+		serverSocket = new ServerSocket(3000);
+		while (true) {
 			System.out.println("Waiting for client system to connect...");
-			Socket socket = serverSocket.accept();
+			Dispatcher dispatcher = () -> new ExecutorThread(serverSocket.accept());
+			dispatcher.dispatch();
+		}
+
+	}
+}
+
+@FunctionalInterface
+interface Dispatcher {
+	public void dispatch() throws IOException;
+}
+
+class ExecutorThread {
+
+	InetAddress clientInetAddress = null;
+	
+	static int clientCount = 0;
+	
+	String clientName = null;
+
+	private Socket clientSocket = null;
+
+	Runnable executorThread = () -> {
+		try {
 			BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
 			System.out
 					.println("Client connected, if all properties set properly, shall we run the batch file? (Yes/No)");
@@ -21,12 +44,21 @@ public class RemoteCommandExecutor {
 			}
 			System.out.println("Sending command...");
 			readLine = "run";
-			PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+			PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
 			printWriter.println(readLine);
 			printWriter.flush();
-			System.out.println("Command sent!");
-		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Command sent, client " + clientInetAddress.getHostAddress() + " served");
+		} catch (IOException exception) {
+			System.out.println("Something wrong while serving the client " + clientName + " Would you please try to connect again?");
 		}
+	};
+
+	public ExecutorThread(Socket argClientSocket) {
+		clientInetAddress = argClientSocket.getInetAddress();
+		clientName = ": Client [" + clientInetAddress.getHostAddress() + "]";
+		clientCount++;
+		System.out.println("New incoming client connection " + clientName);
+		clientSocket = argClientSocket;
+		executorThread.run();
 	}
 }
